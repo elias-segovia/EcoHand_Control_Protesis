980ad0c306431bc0a72c0d27b2af3be9476c9c65
Elías <33992591+elias-segovia@users.noreply.github.com>
2019-08-08 21:42:02 -0300

    Se agrega las secuencias comunes

--- app/src/main/java/com/proyecto/ecohand/control_protesis/Activities/SecuenciasComunesActivity.java
+++ app/src/main/java/com/proyecto/ecohand/control_protesis/Activities/SecuenciasComunesActivity.java
@@ -1,5 +1,6 @@
 package com.proyecto.ecohand.control_protesis.Activities;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -10,76 +11,54 @@ import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.proyecto.ecohand.control_protesis.Adapters.SecuenciaAdapter;
 import com.proyecto.ecohand.control_protesis.Models.Menu;
-import com.proyecto.ecohand.control_protesis.Models.Response.SecuenciaResponse;
-import com.proyecto.ecohand.control_protesis.Services.ApiService;
-import com.proyecto.ecohand.control_protesis.Services.UsuarioService;
+import com.proyecto.ecohand.control_protesis.Models.Secuencia;
 import com.proyecto.ecohand.control_protesis.R;
-import com.proyecto.ecohand.control_protesis.Models.Response.UsuarioResponse;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import com.proyecto.ecohand.control_protesis.Adapters.SecuenciaAdapter;
-import com.proyecto.ecohand.control_protesis.Models.Secuencia;
 
 import customfonts.TextViewSFProDisplayRegular;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
-public class HomeActivity extends AppCompatActivity {
+public class SecuenciasComunesActivity extends Activity {
 
     private ListView listaSecuenciasBtn;
     private ListView listMenu;
-    private ArrayList<String> titles = new ArrayList<>();
     private ArrayList<Secuencia> secuencias = new ArrayList<Secuencia>();
     private android.support.v7.widget.Toolbar toolbar;
     private boolean toolbarVisible = false;
     private ImageView comandoVoz;
-    private ProgressBar spinner;
-    private TextViewSFProDisplayRegular cargaSecuencias;
 
     private TextView comando;
     private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_secuencias_comunes);
 
         listaSecuenciasBtn = findViewById(R.id.list);
         listMenu = findViewById(R.id.listMenuID);
         toolbar = findViewById(R.id.toolbarID);
         comandoVoz = findViewById(R.id.microsfonoID);
         comando = findViewById(R.id.txtComandoID);
-        cargaSecuencias = findViewById(R.id.SubtituloID);
-        spinner = findViewById(R.id.progressBarID);
-        spinner.getIndeterminateDrawable().setColorFilter(Color.rgb(128, 139, 150), PorterDuff.Mode.SRC_IN);
-        spinner.setVisibility(View.VISIBLE);
 
         Menu.SetMenu(this.getBaseContext());
         Menu.setActivity(this);
         listMenu.setAdapter(Menu.getAdapter());
 
+
         SecuenciaAdapter adapter = new SecuenciaAdapter(this, secuencias);
-
         listaSecuenciasBtn.setAdapter(adapter);
-
-        getSecuencias(adapter);
+        cargarSecuencias(adapter);
 
         listMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             Intent intent;
@@ -88,15 +67,15 @@ public class HomeActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0:
-                        intent = new Intent(HomeActivity.this, BluetoothActivity.class);
+                        intent = new Intent(SecuenciasComunesActivity.this, BluetoothActivity.class);
                         startActivity(intent);
                         break;
                     case 1:
-                        intent = new Intent(HomeActivity.this, VozActivity.class);
+                        intent = new Intent(SecuenciasComunesActivity.this, VozActivity.class);
                         startActivity(intent);
                         break;
                     case 2:
-                        intent = new Intent(HomeActivity.this, VersionEcohandActivity.class);
+                        intent = new Intent(SecuenciasComunesActivity.this, VersionEcohandActivity.class);
                         startActivity(intent);
                         break;
                     case 3:
@@ -105,38 +84,21 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
-
-
     }
 
     public void onBackPressed() {
-        Intent intentInicio = new Intent(this, InicioActivity.class);
+        Intent intentInicio = new Intent(this, HomeActivity.class);
         startActivity(intentInicio);
     }
 
-    private void getSecuencias(final SecuenciaAdapter arrayAdapter) {
+    public void cargarSecuencias(final SecuenciaAdapter arrayAdapter) {
+        arrayAdapter.addSecuencia(new Secuencia("Palma Abierta"));
+        arrayAdapter.addSecuencia(new Secuencia("Palma Cerrada"));
+        arrayAdapter.addSecuencia(new Secuencia("Sostener Objeto"));
+        arrayAdapter.addSecuencia(new Secuencia("Pulsar Botón"));
+        arrayAdapter.addSecuencia(new Secuencia("Hacer Click"));
 
-        Call<List<SecuenciaResponse>> call = ApiService.getSecuenciaService().get();
-
-        call.enqueue(new Callback<List<SecuenciaResponse>>() {
-            @Override
-            public void onResponse(Call<List<SecuenciaResponse>> call, Response<List<SecuenciaResponse>> response) {
-
-                for (SecuenciaResponse s : response.body()) {
-                    arrayAdapter.addSecuencia(new Secuencia(s.getNombre()));
-//                    titles.add(s.getNombre());
-                }
-
-                arrayAdapter.notifyDataSetChanged();
-                cargaSecuencias.setVisibility(View.GONE);
-                spinner.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onFailure(Call<List<SecuenciaResponse>> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        arrayAdapter.notifyDataSetChanged();
     }
 
     public void Menu(View view) {
@@ -156,13 +118,13 @@ public class HomeActivity extends AppCompatActivity {
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface arg0, int arg1) {
-                        Intent intent = new Intent(HomeActivity.this, InicioActivity.class);
+                        Intent intent = new Intent(SecuenciasComunesActivity.this, InicioActivity.class);
                         SharedPreferences prefs = getSharedPreferences("PreferenciaUsuario", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = prefs.edit();
                         editor.putBoolean("Registrado", false);
                         editor.commit();
                         finish();
-                        Toast.makeText(HomeActivity.this, "Sesión cerrada!", Toast.LENGTH_LONG).show();
+                        Toast.makeText(SecuenciasComunesActivity.this, "Sesión cerrada!", Toast.LENGTH_LONG).show();
                         startActivity(intent);
                     }
                 });
@@ -206,39 +168,8 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    private void getUsuarios(final ArrayAdapter arrayAdapter) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://ecohand-backend.azurewebsites.net/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        UsuarioService usuarioServices = retrofit.create(UsuarioService.class);
-        Call<List<UsuarioResponse>> call = usuarioServices.get();
-
-        call.enqueue(new Callback<List<UsuarioResponse>>() {
-            @Override
-            public void onResponse(Call<List<UsuarioResponse>> call, Response<List<UsuarioResponse>> response) {
-
-                for (UsuarioResponse u : response.body()) {
-                    titles.add(u.getUsername());
-                }
-
-                arrayAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onFailure(Call<List<UsuarioResponse>> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    public void left(View view){
-        Intent intent = new Intent(HomeActivity.this, SecuenciasComunesActivity.class);
-        startActivity(intent);
-    }
-
     public void right(View view){
-        Intent intent = new Intent(HomeActivity.this, SecuenciasComunesActivity.class);
+        Intent intent = new Intent(SecuenciasComunesActivity.this, HomeActivity.class);
         startActivity(intent);
     }
 }
